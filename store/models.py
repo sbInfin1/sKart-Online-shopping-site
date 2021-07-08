00d210bf8915eefa8cb84e38eb459519c2f0ce62
Sarit Burman <saritb734@gmail.com>
2021-07-08 18:50:11 +0530

    Part2:Cart page done

--- store/models.py
+++ store/models.py
@@ -37,11 +37,35 @@ class Order(models.Model):
     def __str__(self):
         return str(self.transaction_id)
 
+    '''
+    Returns the total value off all items in cart taking into account the quantity
+    of each item
+    '''
+    @property
+    def get_cart_total(self):
+        orderitems = self.orderitem_set.all()
+        total = sum(item.get_total for item in orderitems)
+        return total
+
+    '''
+    Returns the sum total of the number of items of each type
+    '''
+    @property
+    def get_cart_items(self):
+        orderitems = self.orderitem_set.all()
+        total = sum(item.quantity for item in orderitems)
+        return total
+
 class OrderItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, blank=True, null=True)
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
     quantity = models.IntegerField(default=0, null=True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
 
 class ShippingAddress(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
