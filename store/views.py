42a091b4eb8a53113fd039f3cc59e057ac3f7c0c
Sarit Burman <saritb734@gmail.com>
2021-07-09 16:21:26 +0530

    Part3:ajax with csrf_token done

--- store/views.py
+++ store/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import *
+from django.http import JsonResponse
 
 # Create your views here.
 def store(request):
@@ -30,3 +31,6 @@ def checkout(request):
 
     context = {'items':items, 'order': order}
     return render(request, 'store/checkout.html', context)
+
+def updateItem(request):
+    return JsonResponse('Item was added', safe=False)
