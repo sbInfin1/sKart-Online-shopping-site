b38adeaa450deb7635428800ebc0962ae6fb9b51
Sarit Burman <saritb734@gmail.com>
2021-07-09 16:36:42 +0530

    Part3:updateItem() in views.py set up so that user can add items to the cart from the frontend

--- store/views.py
+++ store/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import *
 from django.http import JsonResponse
+import json
 
 # Create your views here.
 def store(request):
@@ -33,4 +34,26 @@ def checkout(request):
     return render(request, 'store/checkout.html', context)
 
 def updateItem(request):
+    data = json.loads(request.body)
+    productId = data['productId']
+    action = data['action']
+
+    print('action: {}, productId: {}'.format(action, productId))
+
+    customer = request.user.customer
+    product = Product.objects.get(id=productId)
+    order, created = Order.objects.get_or_create(customer=customer, complete=False)
+
+    orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
+
+    if action == 'add':
+        orderItem.quantity += 1
+    elif action == 'remove':
+        orderItem.quantity -= 1
+
+    orderItem.save()
+
+    if orderItem.quantity <= 0:
+        orderItem.delete()
+
     return JsonResponse('Item was added', safe=False)
