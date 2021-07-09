b2eb22dcc691b0ef7fc2e50724590bb527dc864e
Sarit Burman <saritb734@gmail.com>
2021-07-09 17:01:21 +0530

    Part3:Up-down arrow buttons in the cart page made interactive

--- static/js/cart.js
+++ static/js/cart.js
@@ -19,7 +19,7 @@ for(var i = 0; i < updateBtns.length; i++){
 function updateUserOrder(productId, action){
   console.log('User is logged in, sending data..')
 
-  var url = 'update_item/'
+  var url = '/update_item/'
 
   fetch(url, {
     method: 'POST',
